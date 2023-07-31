a4811f65e65a85ee848904c3beeb82d961cb5a3d
down4u5 <128249162+down4u5@users.noreply.github.com>
2023-07-31 11:00:57 +0800

    Add files via upload

---
+++ posenettest/sketch.js
@@ -0,0 +1,89 @@
+let video;
+let poseNet;
+let poses = [];
+
+//I don't usually like to overwrite publicly accessible variables, but that's just me
+var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+var cameraStream;
+
+getUserMedia.call(navigator, {
+    video: true,
+    audio: true //optional
+}, function (stream) {
+    /*
+    Here's where you handle the stream differently. Chrome needs to convert the stream
+    to an object URL, but Firefox's stream already is one.
+    */
+    if (window.webkitURL) {
+        video.src = window.webkitURL.createObjectURL(stream);
+    } else {
+        video.src = stream;
+    }
+
+    //save it for later
+    cameraStream = stream;
+
+    video.play();
+});
+
+function setup() {
+  createCanvas(1280,720);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on("pose", function(results) {
+    poses = results;
+    console.log(poses);
+  });
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
+
+function modelReady() {
+    console.log("Model Ready");
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+  drawSkeleton();
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i += 1) {
+    // For each pose detected, loop through all the keypoints
+    const pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j += 1) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      const keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i += 1) {
+    const skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j += 1) {
+      const partA = skeleton[j][0];
+      const partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    }
+  }
+}
